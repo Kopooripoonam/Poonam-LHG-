8d26cf3695e2dd0bfd7cc37cb0dd137325ff2595
Poonam Kopoori <127392253+Kopooripoonam@users.noreply.github.com>
2026-03-14 09:21:53 +0530

    Added solution - LeetHub

---
+++ 8382-no-idea-Hackerrank/8382-no-idea.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n , m = map(int, input().split())
+arr = list(map(int, input().split()))
+set_a = set(map(int,input().split()))
+set_b = set(map(int,input().split()))
+happiness = 0
+for i in arr:
+    if i in set_a:
+        happiness += 1
+    if i in set_b:
+        happiness -= 1
+print(happiness)
